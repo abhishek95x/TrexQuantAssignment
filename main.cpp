47e1b4f4fda286496855e7505bd7739bed85eac9
Abhishek Sharma <abhishekxs@icloud.com>
2022-12-21 09:10:36 +0530

    Reading File line by line

--- main.cpp
+++ main.cpp
@@ -4,13 +4,25 @@
 using namespace std;
 
 int main(){
+    clock_t start = clock();
     // cout<<"Hi Abhishek"<<endl;
-    ifstream is("file.txt");
-    char buff[1];
-    do {
-        is.read(buff,sizeof(buff));
-        //Process the message
-        cout<<buff<<endl;
-    }while(is);
+    ifstream is("/Users/abhishek/Downloads/01302019.NASDAQ_ITCH50");
+
+    string line;
+    while (std::getline(is, line)){
+        cout<<line<<endl;
+    }
+    // char buff[4];
+    // do {
+    //     is.read(buff,sizeof(buff));
+        
+    //     //Process the message
+    //     cout<<buff<<endl;
+    // }while(is);
+
+    clock_t end = clock();
+    double runtime = double(end-start)/CLOCKS_PER_SEC;
+    cout<<"Total Runtime : "<<fixed<<runtime<<setprecision(5)<<endl;
+
     return 0;
 }
